b9f989ce70cae2b4212b9fe147e59f69e86ac15c
adela kartika putri <ridhonasrullah45@gmail.com>
2025-01-08 11:58:17 +0700

    ridho 2

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -1,85 +1,44 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container-sm my-5">
-        <form action="{{ route('employees.store') }}" method="POST">
-            @csrf
-            <div class="row justify-content-center">
-                {{-- Dan Seterusnya --}}
-                <div class="p-5 bg-light rounded-3 border col-xl-6">
-                    <div class="mb-3 text-center">
-                        <i class="bi-person-circle fs-1"></i>
-                        <h4>Create Employee</h4>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                            <label for="firstName" class="form-label">First
-                                Name</label>
-                            <input class="form-control @error('firstName')
-            is-invalid @enderror" type="text"
-                                name="firstName" id="firstName" value="{{ old('firstName') }}"
-                                placeholder="Enter First Name">
-                            @error('firstName')
-                                <div class="text-danger"><small>{{ $message }}</small></div>
-                            @enderror
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label for="lastName" class="form-label">LastName</label>
-                            <input class="form-control @error('lastName')
-            is-invalid @enderror" type="text"
-                                name="lastName" id="lastName" value="{{ old('lastName') }}" placeholder="Enter Last Name">
-                            @error('lastName')
-                                <div class="text-danger"><small>{{ $message }}</small></div>
-                            @enderror
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input class="form-control @error('email') is-invalid
-            @enderror" type="text"
-                                name="email" id="email" value="{{ old('email') }}" placeholder="Enter Email">
-                            @error('email')
-                                <div class="text-danger"><small>{{ $message }}</small></div>
-                            @enderror
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label for="age" class="form-label">Age</label>
-                            <input class="form-control @error('age') is-invalid
-            @enderror" type="text"
-                                name="age" id="age" value="{{ old('age') }}" placeholder="Enter Age">
-                            @error('age')
-                                <div class="text-danger"><small>{{ $message }}</small></div>
-                            @enderror
-                        </div>
-                        <div class="col-md-12 mb-3">
-                            <label for="position" class="form-label">Position</label>
-                            <select name="position" id="position" class="form-select">
-                                @foreach ($positions as $position)
-                                    <option value="{{ $position->id }}"
-                                        {{ old('position') == $position->id ? 'selected' : '' }}>
-                                        {{ $position->code . ' - ' . $position->name }}</option>
-                                @endforeach
-                            </select>
-                            @error('position')
-                                <div class="text-danger"><small>{{ $message }}</small></div>
-                            @enderror
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col-md-6 d-grid">
-                            <a href="{{ route('employees.index') }}"
-                                class="btn
-            btn-outline-dark btn-lg mt-3"><i
-                                    class="bi-arrow-left-circle me-2"></i>Cancel</a>
-                        </div>
-                        <div class="col-md-6 d-grid">
-                            <button type="submit" class="btn btn-dark btn-lg
-            mt-3"><i
-                                    class="bi-check-circle me-2"></i> Save</button>
-                        </div>
-                    </div>
+    <div class="container mt-4">
+        <div class="row mb-0">
+            <div class="col-lg-9 col-xl-10">
+                <h4 class="mb-3">{{ $pageTitle }}</h4>
+            </div>
+            <div class="col-lg-3 col-xl-2">
+                <div class="d-grid gap-2">
+                    <a href="{{ route('employees.create') }}" class="btn
+btn-primary">Create Employee</a>
                 </div>
             </div>
-        </form>
+        </div>
+        <hr>
+        <div class="table-responsive border p-3 rounded-3">
+            <table class="table table-bordered table-hover table-striped
+mb-0 bg-white">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                        <th>Position</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($employees as $employee)
+                        <tr>
+                            <td>{{ $employee->firstname }}</td>
+                            <td>{{ $employee->lastname }}</td>
+                            <td>{{ $employee->email }}</td>
+                            <td>{{ $employee->age }}</td>
+                            <td>{{ $employee->position->name }}</td>
+                            <td>@include('employee.actions')</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
